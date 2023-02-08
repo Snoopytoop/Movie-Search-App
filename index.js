731e4b038dc6bf1e915aff5f7557505335da4168
Snoopytoop <samuelluke1998@gmail.com>
2023-02-08 19:05:52 +0000

    working on js

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-let slider = document.getElementById("range");
-let output = document.getElementById("demo");
-output.innerHTML = slider.value;
+async function main(link) {
+  const movies = await fetch(
+    `https://www.omdbapi.com/?apikey=da79f3d4&s=${link}`
+  );
+  const moviesData = await movies.json();
 
-slider.oninput = function() {
-    output.innerHTML = this.value;
+  const resultsEl = document.getElementById("results");
+
+  if (link === undefined) {
+    resultsEl.innerHTML = "";
+  } else {
+    resultsEl.innerHTML = moviesData.Search.map((movie) =>
+      movieHTML(movie)
+    ).join("");
+    resultsEl.scrollIntoView({behavior: "smooth"});
   }
+}
 
+function movieHTML(movie) {
+  return `<div class="results__card--outer">
+  <div class="results__card--inner">
+    <div class="results__card--bg"></div>
+    <img
+      class="results__img"
+      src="${movie.Poster}"
+      alt=""
+    />
+    <div class="results__description">
+      <h2 class="results__title">${movie.Title}</h2>
+      <h2 class="results__year">${movie.Year}</h2>
+    </div>
+  </div>
+</div>`;
+}
 
+main();
+
+function searchFunction(event) {
+  let searchTitle = event.target.value;
+  main(searchTitle);
+}
